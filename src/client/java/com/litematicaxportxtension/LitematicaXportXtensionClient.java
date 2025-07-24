58c4b9c9e38d002b12bb980604138bd678e90de2
MrTentacleGuy <mariohewera@gmx.de>
2025-07-24 06:17:41 +0200

    REnaming all files

--- src/client/java/com/litematicaxportxtension/LitematicaXportXtensionClient.java
+++ src/client/java/com/litematicaxportxtension/LitematicaXportXtensionClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.litematicaxportxtension;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class LitematicaXportXtensionClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
